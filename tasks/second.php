7cd080fdf2a237ce720097b6fed20eac06a377b7
Tao309 <tao309@mail.ru>
2019-09-30 17:44:40 +0300

    Prepare Task 3

--- tasks/second.php
+++ tasks/second.php
@@ -1,13 +1,43 @@
 <?php
 
-
 $sql = '
 SELECT *
 FROM users
 ORDER BY id
 ';
 
-
 $users = $db->findAll($sql);
 
-print_r($users);
+
+$emailsArray = [];
+
+foreach($users as $row)
+{
+    if(empty($row['email']))
+    {
+        continue;
+    }
+
+    $emails = array_unique(explode(',',$row['email']));
+
+    foreach($emails as $email)
+    {
+        if(!isset($emailsArray[$email]))
+        {
+            $emailsArray[$email] = [
+                'count' => 0,
+                'users' => [],
+            ];
+        }
+
+        $emailsArray[$email]['count']++;
+        $emailsArray[$email]['users'][] = [
+            'id' => $row['id'],
+            'name' => $row['name'],
+        ];
+
+    }
+}
+
+
+print_r($emailsArray);
